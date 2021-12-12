d222a0440de35e634063a99e4fa69a58ed1b1257
Hoodyman1 <frosty123dyusha@gmail.com>
2021-12-12 16:56:52 +0300

    после убийства ножки и ключи пропадают сразу

--- visualisation.py
+++ visualisation.py
@@ -55,9 +55,15 @@ def visual_player(screen, width, height, data_player, objects_player, objects_en
         new_objects_enemy = []
         new_objects_enemy.append(revival)
         objects_enemy.append(new_objects_enemy)
+
+        # просто закрашиваем неугодные элементы
         pygame.draw.rect(screen, (255, 255, 255),
                          (width / 2 + 5, height / 3 + 5, width / 2 - 5,
                           height * 2 / 3))  # здесь может быть ошибка с флажком
+
+        pygame.draw.rect(screen, (255, 255, 255),
+                         (width / 2 + 5, height * 4 / 5, width / 2 - 5,
+                          height / 5 - 5))  # здесь может быть ошибка с закрашиванием
         revival.draw()
 
         visual_parts(width, height, objects_enemy, objects_player, [len(objects_player) - 2, len(objects_player) - 3],
@@ -222,7 +228,11 @@ def visual_enemy(screen, width, height, data_enemy, objects_enemy, objects_playe
         new_objects_player = []
         new_objects_player.append(revival)
         objects_player.append(new_objects_player)
+        # закрашиваем неугодные элементы
         pygame.draw.rect(screen, (255, 255, 255), (0, height * 1 / 3 + 5, width / 2, height * 2 / 3))
+        pygame.draw.rect(screen, (255, 255, 255),
+                         (0, height * 4 / 5, width / 2 - 5,
+                          height / 5 - 5))  # здесь может быть ошибка с закрашиванием
         revival.draw()
 
         visual_parts(width, height, objects_enemy, objects_player, [len(objects_player) - 2, len(objects_player) - 3],
